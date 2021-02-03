e8adc9791aaa0a22558baeb2b6ca3a36f3a4f410
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-02-03 15:09:07 +0200

    changed files in ExamPreparation

---
+++ 03JavaAdvanced/09ExamPreparation/Exam23Oct2019/Ex01MakeASalad_23Oct19.java
@@ -0,0 +1,58 @@
+package bg.softuni.javaadvanced;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Ex01MakeASalad_23Oct19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] vegetables = scanner.nextLine().split("\\s+");
+        String[] calories = scanner.nextLine().split("\\s+");
+
+        ArrayDeque<String> vegetablesQueue = new ArrayDeque<>();
+        for (String vegetable : vegetables) {
+            vegetablesQueue.offer(vegetable);
+        }
+
+        ArrayDeque<Integer> caloriesStack = new ArrayDeque<>();
+        for (String calorie : calories) {
+            caloriesStack.push(Integer.parseInt(calorie));
+        }
+
+        while (!caloriesStack.isEmpty() && !vegetablesQueue.isEmpty()) {
+            int salad = caloriesStack.peek();
+
+            while (salad > 0 && !vegetablesQueue.isEmpty()){
+                String vegetable = vegetablesQueue.poll();
+
+                switch(vegetable){
+                    case "tomato":
+                        salad -= 80;
+                        break;
+                    case "carrot":
+                        salad -= 136;
+                        break;
+                    case "lettuce":
+                        salad -= 109;
+                        break;
+                    case "potato":
+                        salad -= 215;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.out.print(caloriesStack.pop() + " ");
+        }
+        System.out.println();
+
+        while (!caloriesStack.isEmpty()){
+            System.out.print(caloriesStack.pop() + " ");
+        }
+
+        while (!vegetablesQueue.isEmpty()){
+            System.out.print(vegetablesQueue.poll() + " ");
+        }
+    }
+}
