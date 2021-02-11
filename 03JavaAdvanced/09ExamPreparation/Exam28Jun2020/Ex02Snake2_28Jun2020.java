bd1a8e966105109bef890eaa32686ccd2482c9af
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-02-11 12:29:41 +0200

    added 02Snake & 03Parking into Exam28Jun2020

---
+++ 03JavaAdvanced/09ExamPreparation/Exam28Jun2020/Ex02Snake2_28Jun2020.java
@@ -0,0 +1,112 @@
+package bg.softuni.javaadvanced;
+
+import java.util.Scanner;
+
+public class Ex02Snake2_28Jun2020 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+        char[][] matrix = scanMatrix(scanner, size);
+        int[]snake = locateSnake(matrix);
+        int row = snake[0];
+        int col = snake[1];
+        int food = 0;
+
+        while (food < 10) {
+            String command = scanner.nextLine();
+            matrix[row][col] = '.';
+            switch (command){
+                case "up":
+                    row--;
+                    break;
+                case "down":
+                    row++;
+                    break;
+                case "left":
+                    col--;
+                    break;
+                case "right":
+                    col++;
+                    break;
+                default:
+                    break;
+            }
+            if (!isInBounds(row, col, matrix)){
+                break;
+            }
+            char symbol = matrix[row][col];
+            switch (symbol){
+                case '*':
+                    matrix[row][col] = 'S';
+                    food++;
+                    break;
+                case 'B':
+                    matrix[row][col] = '.';
+                    int[] borrowOut = snakeGoesOutOnBorrow(matrix);
+                    row = borrowOut[0];
+                    col = borrowOut[1];
+                    matrix[row][col] = 'S';
+                    break;
+                default:
+                    matrix[row][col] = 'S';
+                    break;
+            }
+        }
+
+        if (food == 10){
+            System.out.println("You won! You fed the snake.");
+        } else {
+            System.out.println("Game over!");
+        }
+        System.out.printf("Food eaten: %d%n", food);
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[] snakeGoesOutOnBorrow(char[][] matrix) {
+        int[] borrowOut = new int[2];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == 'B'){
+                    borrowOut[0] = row;
+                    borrowOut[1] = col;
+                }
+            }
+        }
+        return borrowOut;
+    }
+
+    private static boolean isInBounds(int row, int col, char[][]matrix){
+        return row >= 0 && row < matrix.length && col >= 0 && col < matrix[row].length;
+    }
+
+    private static int[] locateSnake(char[][] matrix) {
+        int[] snake = new int[2];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == 'S'){
+                    snake[0] = row;
+                    snake[1] = col;
+                }
+            }
+        }
+        return snake;
+    }
+
+    private static char[][] scanMatrix(Scanner scanner, int size) {
+        char[][]matrix = new char[size][];
+        for (int row = 0; row < size; row++) {
+            matrix[row] = scanner.nextLine().toCharArray();
+        }
+        return matrix;
+    }
+}
---
+++ 03JavaAdvanced/09ExamPreparation/Exam28Jun2020/parking/Parking.java
@@ -0,0 +1,79 @@
+package parking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parking {
+    private String type;
+    private int capacity;
+    private List<Car> data;
+
+    public Parking(String type, int capacity) {
+        this.type = type;
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Car car){
+        if (this.data.size() < capacity){
+            this.data.add(car);
+        }
+    }
+
+    public boolean remove(String manufacturer, String model){
+        return this.data.removeIf(car -> car.getManufacturer().equals(manufacturer) && car.getModel().equals(model));
+    }
+
+    public Car getLatestCar(){
+        return this.data.stream()
+                .sorted((f, s) -> Integer.compare(s.getYear(), f.getYear()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Car getLatestCar2() {
+        Car result = null;
+
+        for (Car car : data) {
+            if (result == null || result.getYear() < car.getYear()) {
+                result = car;
+            }
+        }
+
+        return result;
+    }
+
+    public Car getCar(String manufacturer, String model){
+        Car carByManufacturerAndModel = null;
+        for (Car car : this.data) {
+            if (car.getManufacturer().equals(manufacturer) && car.getModel().equals(model)){
+                carByManufacturerAndModel = car;
+                break;
+            }
+        }
+        return carByManufacturerAndModel;
+    }
+
+    public Car getCar2(String manufacturer, String model) {
+        for (Car car : data) {
+            if (car.getManufacturer().equals(manufacturer)
+                    && car.getModel().equals(model)) {
+                return car;
+            }
+        }
+        return null;
+    }
+
+    public int getCount(){
+        return this.data.size();
+    }
+
+    public String getStatistics(){
+        StringBuilder sb = new StringBuilder(String.format("The cars are parked in %s:", this.type))
+                .append(System.lineSeparator());
+        for (Car car : this.data) {
+            sb.append(car).append(System.lineSeparator());
+        }
+        return sb.toString().trim();
+    }
+}
